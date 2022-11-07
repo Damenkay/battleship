b875fa1eb68c05ea108fd69ab7c6f8ccc7cd3855
Damenkay <damenkangnaan@yahoo.com>
2022-11-07 20:44:42 +0100

    second commit

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ var view = {
     },
     displayMiss:function(location) {
         var cell = document.getElementById(location);
-        cell.setAttribute("class", "hit");
+        cell.setAttribute("class", "miss");
     },
     displayHit:function(location) {
         var cell = document.getElementById(location)
-        cell.setAttribute("class","miss")
+        cell.setAttribute("class","hit")
     }
 }
 var model = {
@@ -21,13 +21,59 @@ var model = {
             { locations: ["24", "34", "44"], hits: ["", "", ""] },
             { locations: ["10", "11", "12"], hits: ["", "", ""] }],
     fire: function (guess) {
-    
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if (index>=0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess)
+                view.displayMessage("HIT!!!")
+                if (this.isSunk(ship)) {
+                    view.displayMessage("you sank my battleship");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+            
+        }
+        view.displayMiss(guess)
+        view.displayMessage("You Missed!!!")
+        return false;
         
+    },
+    isSunk:function (ship) {
+        for (var i = 0; i< this.shipLength; i++) {
+            if (ship.hits !== "hit" ) {
+                return false;
+            }
+            
+        }
+        return true;
     }
-}
+};
+
+// model.fire("53"); // miss
+// model.fire("05"); // miss
+// model.fire("15"); // miss
+// model.fire("20"); // miss
+// model.fire("45"); // miss
+// model.fire("30"); // miss
 
 
-iiiuy
+
+
+
+// model.fire("06"); // hit
+// model.fire("16"); // hit
+// model.fire("26"); // hit
+
+// model.fire("34"); // hit
+// model.fire("24"); // hit
+// model.fire("44"); // hit
+
+// model.fire("12"); // hit
+// model.fire("11"); // hit
+// model.fire("10"); //
 
 // view.displayHit("00")
 // view.displayMiss("34")
